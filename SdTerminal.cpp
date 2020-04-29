bbcb78dc22e3ca5bf8076bc607726a3b6f103a96
PB2 <gitstuff2@gmail.com>
2020-04-28 21:35:19 -0400

    Add files via upload

---
+++ SdTerminal.cpp
@@ -0,0 +1,253 @@
+#include "SdLog.h"
+
+
+
+
+bool Logger::begin(Log_Meta& logMeta, Stream& stream, const uint16_t& timeout)
+{
+	metaP = &logMeta;
+	_serial = &stream;
+	_timeout = timeout;
+
+	msTimer.begin(_timeout);
+
+	numColumns = numOccur(metaP->headerRow, sizeof(metaP->headerRow)) + 1;
+
+	return init();
+}
+
+
+
+
+bool Logger::init()
+{
+	unsigned int count = 1;
+
+	if (sd.begin())
+	{
+		sprintf(filename, metaP->nameTemplate, count);
+
+		while (sd.exists(filename))
+		{
+			count++;
+			sprintf(filename, metaP->nameTemplate, count);
+		}
+
+		myFile.open(filename, FILE_WRITE);
+		myFile.println(metaP->nameTemplate);
+		myFile.close();
+
+		initialized = true;
+	}
+	else
+	{
+		initialized = false;
+		_serial->println("SD Card Init Failed");
+	}
+
+	return initialized;
+}
+
+
+
+
+void Logger::log(const float* data, const uint16_t& dataLen)
+{
+	myFile.open(filename, FILE_WRITE);
+
+	for (uint16_t i=0; i<dataLen; i++)
+	{
+		myFile.print(data[i]);
+
+		if ((i != (dataLen - 1)) && (i != (numColumns - 1)))
+			myFile.print(',');
+		else if (i == (numColumns - 1))
+			break;
+	}
+
+	myFile.println();
+	myFile.close();
+}
+
+
+
+
+void Logger::log(const char* data)
+{
+	myFile.open(filename, FILE_WRITE);
+	myFile.println(data);
+	myFile.close();
+}
+
+
+
+
+
+void Logger::handleCmds()
+{
+	if (_serial->available())
+	{
+		char input[50] = { '\0' };
+
+		readInput(input, sizeof(input));
+
+		if (!strcmp(input, "ls"))
+		{
+			_serial->println(F("--------------------------------------------------"));
+
+			root = sd.open("/");
+			printDirectory(root, 0);
+
+			_serial->println(F("--------------------------------------------------"));
+			_serial->println();
+		}
+		else if (strstr(input, "rm "))
+		{
+			char* p = strstr(input, "rm ") + 3;
+
+			char fileName[40] = { '\0' };
+
+			for (byte i = 0; i < sizeof(fileName); i++)
+			{
+				fileName[i] = *p;
+
+				if (*p == '\0')
+					break;
+
+				p++;
+			}
+
+			if (sd.exists(fileName))
+			{
+				_serial->print("Deleting File: ");
+				_serial->println(fileName);
+
+				myFile.open(fileName, O_WRITE);
+				myFile.remove();
+			}
+			else if (!strcmp(fileName, "all"))
+			{
+				char fileName_[20];
+
+				while (true)
+				{
+					File entry = root.openNextFile();
+
+					if (!entry)
+						break;
+
+					entry.getName(fileName_, sizeof(fileName_));
+
+					if (!entry.isDirectory())
+					{
+						_serial->print("Deleting: ");
+						_serial->println(fileName_);
+
+						myFile.open(fileName_, O_WRITE);
+						myFile.remove();
+					}
+
+					entry.close();
+				}
+			}
+			else
+			{
+				_serial->print(fileName);
+				_serial->println(" not found");
+			}
+		}
+		else if (sd.exists(input))
+		{
+			_serial->println(F("--------------------------------------------------"));
+			_serial->print(input); _serial->println(" found:");
+
+			myFile.open(input, FILE_READ);
+
+			int data;
+			while ((data = myFile.read()) >= 0)
+				_serial->write(data);
+
+			myFile.close();
+
+			_serial->println(F("--------------------------------------------------"));
+			_serial->println();
+		}
+		else
+			_serial->println('?');
+	}
+}
+
+
+
+
+void Logger::readInput(char input[], const uint8_t& inputSize)
+{
+	uint8_t i = 0;
+	
+	msTimer.reset();
+
+	while (_serial->available())
+	{
+		char c = _serial->read();
+
+		if (msTimer.fire())
+			break;
+		else if (c == '\n')
+			break;
+		else if (i >= inputSize)
+			break;
+
+		input[i] = c;
+		i++;
+	}
+}
+
+
+
+
+void Logger::printDirectory(File dir, const int& numTabs)
+{
+	char fileName[20];
+
+	while (true)
+	{
+		File entry = dir.openNextFile();
+
+		if (!entry)
+			break;
+
+		for (uint8_t i=0; i <numTabs; i++)
+			_serial->print('\t');
+
+		entry.getName(fileName, sizeof(fileName));
+		_serial->print(fileName);
+
+		if (entry.isDirectory())
+		{
+			_serial->println("/");
+			printDirectory(entry, numTabs + 1);
+		}
+		else
+		{
+			_serial->print("\t\t");
+			_serial->println(entry.size(), DEC);
+		}
+
+		entry.close();
+	}
+}
+
+
+
+
+uint8_t Logger::numOccur(char* input, const uint16_t& len, const char& target)
+{
+	char* p = input;
+	uint8_t count = 0;
+
+	while (*p != '\0')
+		if (*p++ == target)
+			count++;
+
+	return count;
+}
---
+++ SdTerminal.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Arduino.h"
+#include "SdFat.h"
+#include "FireTimer.h"
+
+
+
+
+const uint8_t MAX_FILENAME_LEN = 40;
+const uint8_t MAX_HEADER_LEN = 150;
+
+
+
+
+struct Log_Meta {
+	char nameTemplate[MAX_FILENAME_LEN];
+	char headerRow[MAX_HEADER_LEN];
+};
+
+
+
+
+class Logger
+{
+public:
+	bool initialized;
+	uint8_t numColumns;
+
+
+
+
+	bool begin(Log_Meta& logMeta, Stream& stream=Serial, const uint16_t& timeout=100);
+	bool init();
+	void log(const float* data, const uint16_t& dataLen);
+	void log(const char* data);
+	void handleCmds();
+
+
+
+
+private:
+	Stream* _serial;
+	FireTimer msTimer;
+	Log_Meta* metaP;
+	SdFatSdioEX sd;
+	SdFile myFile;
+	File root;
+
+	char filename[MAX_FILENAME_LEN];
+
+	uint16_t _timeout;
+
+
+
+
+	void readInput(char input[], const uint8_t& inputSize);
+	void printDirectory(File dir, const int& numTabs);
+	uint8_t numOccur(char* input, const uint16_t& len, const char& target=',');
+};
